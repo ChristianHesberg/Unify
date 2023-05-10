050f15ac0263ee2b9be4e0ac95d8071d34edfc9d
magn20 <magnum.overgaar@hotmail.com>
2023-05-10 13:48:24 +0200

    account screen

--- lib/Screens/AccountScreen.dart
+++ lib/Screens/AccountScreen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:unify/Widgets/user_text.dart';
 
 import '../Widgets/user_text_field.dart';
 
@@ -16,7 +17,6 @@ class _AccountScreenState extends State<AccountScreen> {
       appBar: AppBar(
         title: Text("Account Info"),
         backgroundColor: Colors.black,
-
       ),
       body: _accountScreen(),
     );
@@ -27,20 +27,56 @@ class _AccountScreenState extends State<AccountScreen> {
   final _formKey = GlobalKey<FormState>();
   TextEditingController nameController = TextEditingController();
 
-
   Widget _accountScreen() {
-    return Padding(
-      padding: EdgeInsets.all(12.0),
+    return SingleChildScrollView(
       child: Column(
         children: [
+          SizedBox(
+            height: 300,
+            width: 200,
+            child: Stack(
+              children: [
+                Align(
+                  alignment: Alignment.center,
+                  child: ClipRRect(
+                    borderRadius: BorderRadius.circular(100), // Image border
+                    child: SizedBox.fromSize(
+                      size: Size.fromRadius(120), // Image radius
+                      child: Image.network(
+                          'https://images.pexels.com/photos/415829/pexels-photo-415829.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+                          fit: BoxFit.cover),
+                    ),
+                  ),
+                ),
+                Positioned(
+                  bottom: 25,
+                    right: 10,
+                    child: ElevatedButton(
+                      style: ButtonStyle(
+                        shape:
+                        MaterialStateProperty.all<RoundedRectangleBorder>(
+                          RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(300.0),
+                          ),
+                        ),
+                      ),
+                      onPressed: () {},
+                      child: const Icon(Icons.edit),
+                    )),
+              ],
+            ),
+          ),
           Form(
               key: _formKey,
-              child: Column(children: [
-                UserTextField(
-                  controller: nameController,
-                  label: "name",
-                )
-              ])),
+              child: Padding(
+                padding: const EdgeInsets.all(30.0),
+                child: Column(children: [
+                  UserTextField(
+                    controller: nameController,
+                    label: "name",
+                  )
+                ]),
+              )),
           ElevatedButton(
               onPressed: () {
                 if (_formKey.currentState!.validate()) {
