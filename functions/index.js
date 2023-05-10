dd91b2b4bfb03efae23abf59a6c2e350c6cd0154
ChristianHesberg <chri59kx@easv365.dk>
2023-05-10 11:09:46 +0200

    using geofire, but doesnt work

--- functions/index.js
+++ functions/index.js
@@ -1,7 +1,7 @@
 const functions = require("firebase-functions");
 const admin = require("firebase-admin");
 admin.initializeApp({projectId: 'unify-ef8e0'});
-const geofire = require('geofire-common');
+const geoFirestore = require('geofirestore');
 
 const app = require('express')();
 const cors = require('cors');
@@ -11,21 +11,43 @@ app.use(cors());
 app.get('/matches/userAge/:userAge/maxAge/:maxAge/minAge/:minAge/matchGender/:matchGender/genderPrefs/:genderPrefs', async (req, res) =>{
 
     const genderPreferences = req.params.genderPrefs.split("-");
-    results = [];
-    const getResult =
-        await admin.firestore().collection('users')
+    let results = [];
+    /*const getResult =
+        admin.firestore().collection('users')
         .where(req.params.matchGender, "==", true)
         .where('maxAgePreference', ">=", Number(req.params.userAge))
         .where('minAgePreference', '<=', Number(req.params.userAge))
         .where('age', '<=', Number(req.params.maxAge))
         .where('age', '>=', Number(req.params.minAge))
-        .where('gender', 'in', genderPreferences)
-    .get();
-    getResult.forEach(doc =>{
-        results.push(doc.data());
-    })
+        .where('gender', 'in', genderPreferences);
+    //.get();
+    //getResult.forEach(doc =>{
+    //    results.push(doc.data());
+    //});
+
+     */
+    const lat = 37.3882733;
+    const long = -122.0778017;
+    const center = new admin.firestore.GeoPoint(lat,long);
+
+    const firestore = admin.firestore();
+    const Geofirestore = geoFirestore.initializeApp(firestore);
+    const geocollection = Geofirestore.collection('users')
+        .where(req.params.matchGender, "==", true)
+        .where('maxAgePreference', ">=", Number(req.params.userAge))
+        .where('minAgePreference', '<=', Number(req.params.userAge))
+        .where('age', '<=', Number(req.params.maxAge))
+        .where('age', '>=', Number(req.params.minAge))
+        .where('gender', 'in', genderPreferences);
+    const queryCollection = geocollection.near({
+        center: center,
+        radius : 50,
+    });
+
+
+    const usersNearby = await queryCollection.get();
+    usersNearby.forEach(doc => results.push(doc.data()));
     res.send(results);
-    geofire.geohashQueryBounds()
 });
 
 exports.api = functions.https.onRequest(app);
