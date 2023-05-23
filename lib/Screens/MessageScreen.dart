f54b06862a20f382798f5d0fe0a2183ef98ffea9
Victorkaffekande <victor.gugerel@gmail.com>
2023-05-23 21:11:59 +0200

    cool chat bubbles

--- lib/Screens/MessageScreen.dart
+++ lib/Screens/MessageScreen.dart
@@ -1,3 +1,6 @@
+import 'package:chat_bubbles/bubbles/bubble_normal.dart';
+import 'package:chat_bubbles/bubbles/bubble_special_one.dart';
+import 'package:chat_bubbles/chat_bubbles.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_ui_firestore/firebase_ui_firestore.dart';
 import 'package:flutter/material.dart';
@@ -12,80 +15,85 @@ import '../user_service.dart';
 class MessageScreen extends StatelessWidget {
   final padding = 8.0;
   final Chat chat;
+  final ScrollController controller = ScrollController();
 
-  const MessageScreen({required this.chat, super.key});
+  MessageScreen({required this.chat, super.key});
 
+//TODO SCROLL CONTROLLER SCROLL TO BOTTOM ON SEND MES
   @override
   Widget build(BuildContext context) {
     final chatService = Provider.of<ChatService>(context);
+    final userService = Provider.of<UserService>(context);
+    var uId = userService.user!.id;
     return Scaffold(
       appBar: AppBar(title: const Text("Chats"), backgroundColor: Colors.black),
-      body: Stack(
+      body: Column(
         children: [
-          FirestoreListView<Message>(
-            query: chatService.getMessages(chat),
-            itemBuilder: (context, doc) {
-              final message = doc.data();
-              return SingleChildScrollView(
-                child: Column(children: [
-                  sender(message),
-                  textBubble(message),
-                  SizedBox(height: padding * 2),
-                ]),
-              );
-            },
+          Expanded(
+            child: FirestoreListView<Message>(
+              controller: controller,
+              query: chatService.getMessages(chat),
+              itemBuilder: (context, doc) {
+                final message = doc.data();
+                return SingleChildScrollView(
+                  child: Column(children: [
+                    sender(message, uId),
+                    _createBubble(message, uId),
+                    SizedBox(height: padding * 2),
+                  ]),
+                );
+              },
+            ),
           ),
-          Align(alignment: Alignment.bottomCenter, child: messageInput(context))
+          _buildMessageBar(chatService, userService)
         ],
       ),
     );
   }
 
-  Widget sender(Message message) {
+  _createBubble(Message message, String uId) {
+    if (message.sender.uid == uId) {
+      return BubbleNormal(
+        textStyle: const TextStyle(fontSize: 20, color: Colors.white),
+        text: message.content,
+        color: const Color(0xFF1B97F3),
+        isSender: true,
+      );
+    }
+    return BubbleNormal(
+      text: message.content,
+      textStyle: const TextStyle(fontSize: 20),
+      color: Color(0xFFE8E8EE),
+      isSender: false,
+    );
+  }
+
+  Widget sender(Message message, String uId) {
+    var senderId = message.sender.uid;
     return Padding(
       padding: EdgeInsets.symmetric(horizontal: padding),
       child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        mainAxisSize: MainAxisSize.max,
+        mainAxisAlignment:
+            uId == senderId ? MainAxisAlignment.end : MainAxisAlignment.start,
         children: [
           Text(message.sender.displayName),
+          const SizedBox(
+            width: 10,
+          ), //spacing
           if (message.timestamp != null) Text(since(message.timestamp!))
         ],
       ),
     );
   }
 
-  Widget textBubble(Message message) {
-    return SizedBox(
-      width: double.infinity,
-      child: Card(
-        child: Padding(
-          padding: EdgeInsets.all(padding),
-          child: Text(message.content),
-        ),
-      ),
+  _buildMessageBar(ChatService chatService, UserService userService) {
+    return MessageBar(
+      onSend: (value) async {
+        if (value.isEmpty) return;
+        await chatService.sendMessage(userService.user!, chat, value);
+      },
     );
   }
-
-  Widget messageInput(BuildContext context) {
-    final userService = Provider.of<UserService>(context);
-    final chatService = Provider.of<ChatService>(context);
-    return Padding(
-      padding: EdgeInsets.all(padding),
-      child: TextField(
-        keyboardType: TextInputType.text,
-        onSubmitted: (value) {
-          if (value.isEmpty) return;
-          chatService.sendMessage(userService.user!, chat, value);
-        },
-        decoration: const InputDecoration(
-          border: OutlineInputBorder(),
-          suffixIcon: Icon(Icons.send),
-        ),
-      ),
-    );
-  }
-
 
   String since(DateTime timestamp) {
     final diff = DateTime.now().difference(timestamp);
--- lib/chat_service.dart
+++ lib/chat_service.dart
@@ -11,14 +11,13 @@ import 'models/chat.dart';
 import 'models/message.dart';
 import 'models/sender.dart';
 
-class ChatService{
+class ChatService {
   static const chats = 'chats';
   static const messages = 'messages';
   static const timestamp = 'timestamp';
   final _firestore = FirebaseFirestore.instance;
 
-
-  ChatService(){
+  ChatService() {
     _firestore.useFirestoreEmulator('localhost', 8080);
   }
 
@@ -27,56 +26,52 @@ class ChatService{
         .collection(chats)
         .where(ChatKeys.userIds, arrayContains: user.uid)
         .withConverter(
-      fromFirestore: (snapshot, options) =>
-          Chat.fromMap(snapshot.id, snapshot.data()!),
-      toFirestore: (value, options) => value.toMap(),
-    )
+          fromFirestore: (snapshot, options) =>
+              Chat.fromMap(snapshot.id, snapshot.data()!),
+          toFirestore: (value, options) => value.toMap(),
+        )
         .snapshots()
         .map((querySnapshot) => querySnapshot.docs.map((e) => e.data()));
   }
 
-  postChat(AppUser user1, AppUser user2) async{
+  postChat(AppUser user1, AppUser user2) async {
     var token = await FirebaseAuth.instance.currentUser!.getIdToken();
-    await http.post(
-      Uri.parse('${BaseUrl.baseUrl}chat'),
-      headers: <String, String>{
-        HttpHeaders.authorizationHeader: token,
-        'Content-Type': 'application/json; charset=UTF-8',
-      },
-      body: json.encode({
-        'uid2': user2.id,
-        'displayName1': user1.name,
-        'displayName2': user2.name
-      })
-    );
+    await http.post(Uri.parse('${BaseUrl.baseUrl}chat'),
+        headers: <String, String>{
+          HttpHeaders.authorizationHeader: token,
+          'Content-Type': 'application/json; charset=UTF-8',
+        },
+        body: json.encode({
+          'uid2': user2.id,
+          'displayName1': user1.name,
+          'displayName2': user2.name
+        }));
   }
 
-  Query<Message> getMessages(Chat chat){
+  Query<Message> getMessages(Chat chat) {
     return _firestore
         .collection(chats)
         .doc(chat.id)
         .collection(messages)
         .orderBy(MessageKeys.timestamp)
         .withConverter(
-      fromFirestore: (snapshot, options) =>
-          Message.fromMap(snapshot.id, snapshot.data()!),
-      toFirestore: (value, options) => value.toMap(),
-    );
+          fromFirestore: (snapshot, options) =>
+              Message.fromMap(snapshot.id, snapshot.data()!),
+          toFirestore: (value, options) => value.toMap(),
+        );
   }
 
   sendMessage(AppUser user, Chat chat, String message) async {
     var token = await FirebaseAuth.instance.currentUser!.getIdToken();
-    await http.post(
-      Uri.parse('${BaseUrl.baseUrl}message'),
+    await http.post(Uri.parse('${BaseUrl.baseUrl}message'),
         headers: <String, String>{
           HttpHeaders.authorizationHeader: token,
           'Content-Type': 'application/json; charset=UTF-8',
         },
-      body: json.encode({
-        ChatKeys.chatId: chat.id,
-        MessageKeys.content: message,
-        MessageKeys.sender: user.name ?? 'test',
-      })
-    );
+        body: json.encode({
+          ChatKeys.chatId: chat.id,
+          MessageKeys.content: message,
+          MessageKeys.sender: user.name ?? 'test',
+        }));
   }
 }
