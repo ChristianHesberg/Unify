2852effa547b7a481c718859e51000395a81a3ee
magn20 <magnum.overgaar@hotmail.com>
2023-05-23 14:44:28 +0200

    refactor

--- lib/Screens/PreferenceScreen.dart
+++ lib/Screens/PreferenceScreen.dart
@@ -10,6 +10,7 @@ import '../user_service.dart';
 
 class PreferenceScreen extends StatefulWidget {
   const PreferenceScreen({Key? key}) : super(key: key);
+
   @override
   State<PreferenceScreen> createState() => _PreferenceScreenState();
 }
@@ -20,26 +21,16 @@ class _PreferenceScreenState extends State<PreferenceScreen> {
   Map<String, bool> genderMap = {};
   SfRangeValues? rangeValues;
 
-
-
-  @override
-  void initState() {
-    super.initState();
-    rangeValues = SfRangeValues(user.minAgePreference, user.maxAgePreference);
-    distance =  user.locationPreference.toDouble();
-  }
-
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("Preferences"),backgroundColor: Colors.black),
+      appBar: AppBar(title: Text("Preferences"), backgroundColor: Colors.black),
       body: Consumer<UserService>(
         builder: (context, value, child) {
-          if(value.user == null){
+          if (value.user == null) {
             value.getUser();
             return Center(child: CircularProgressIndicator());
-          }else{
+          } else {
             this.user = value.user!;
             return _buildView();
           }
@@ -47,28 +38,51 @@ class _PreferenceScreenState extends State<PreferenceScreen> {
       ),
     );
   }
-bool loading = false;
+
+  bool loading = false;
+
   SingleChildScrollView _buildView() {
     return SingleChildScrollView(
       child: Padding(
         padding: EdgeInsets.all(8.0),
-        child: loading == false ? Column(
-          children: [
-            GenderCheckBoxes(men: user.genderPreferences.contains("male"), women: user.genderPreferences.contains("female"), other: user.genderPreferences.contains("other"), onClick: _handleGenderCheckBoxes),
-
-            AgeSlider(ageRangeValues: SfRangeValues(user.minAgePreference, user.maxAgePreference), onSlide: _handleOnSlide),
-
-            DistanceSlider(onSlide: _handleDistanceSlider,startingValue: user.locationPreference.toDouble(),),
-
-            ElevatedButton(onPressed: () async {
-              loading = true;
-              final userService = Provider.of<UserService>(context, listen:false);
-              await userService.updateUserPreference(rangeValues!.start, rangeValues!.end, genderMap["Woman"] ?? user.genderPreferences.contains("female"), genderMap["Man"] ?? user.genderPreferences.contains("male"), genderMap["Other"] ?? user.genderPreferences.contains("other"), distance!.round());
-              loading = false;
-              setState(() {});
-            }, child: Text("Update"))
-          ],
-        ) : CircularProgressIndicator(),
+        child: loading == false
+            ? Column(
+                children: [
+                  GenderCheckBoxes(
+                      men: user.genderPreferences.contains("male"),
+                      women: user.genderPreferences.contains("female"),
+                      other: user.genderPreferences.contains("other"),
+                      onClick: _handleGenderCheckBoxes),
+                  AgeSlider(
+                      ageRangeValues: SfRangeValues(
+                          user.minAgePreference, user.maxAgePreference),
+                      onSlide: _handleOnSlide),
+                  DistanceSlider(
+                    onSlide: _handleDistanceSlider,
+                    startingValue: user.locationPreference.toDouble(),
+                  ),
+                  ElevatedButton(
+                      onPressed: () async {
+                        loading = true;
+                        final userService =
+                            Provider.of<UserService>(context, listen: false);
+                        await userService.updateUserPreference(
+                            rangeValues?.start ?? user.minAgePreference,
+                            rangeValues?.end ?? user.maxAgePreference,
+                            genderMap["Woman"] ??
+                                user.genderPreferences.contains("female"),
+                            genderMap["Man"] ??
+                                user.genderPreferences.contains("male"),
+                            genderMap["Other"] ??
+                                user.genderPreferences.contains("other"),
+                            distance?.round() ?? user.locationPreference);
+                        loading = false;
+                        setState(() {});
+                      },
+                      child: Text("Update"))
+                ],
+              )
+            : CircularProgressIndicator(),
       ),
     );
   }
@@ -76,7 +90,6 @@ bool loading = false;
   _handleDistanceSlider(double val) {
     distance = val;
   }
-
 
   _handleGenderCheckBoxes(Map<String, bool> values) {
     genderMap = values;
